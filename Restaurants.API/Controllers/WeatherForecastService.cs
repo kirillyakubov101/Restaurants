33e5902ba7dad6bc4e716be3841bff24df83c847
kirill.yakubov <kirillyakubov101@gmail.com>
2025-01-18 17:27:58 -0500

    Add project files.

---
+++ Restaurants.API/Controllers/WeatherForecastService.cs
@@ -0,0 +1,25 @@
+﻿namespace Restaurants.API.Controllers
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get(int numberOfResults, int minTempDeg, int maxTempDeg);
+    }
+
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        public IEnumerable<WeatherForecast> Get(int numberOfResults, int minTempDeg, int maxTempDeg)
+        {
+            return Enumerable.Range(1, numberOfResults).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(minTempDeg, maxTempDeg),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray();
+            
+        }
+    }
+}
---
+++ Restaurants.API/Program.cs
@@ -0,0 +1,22 @@
+using Restaurants.API.Controllers;
+using Restaurants.Infrastructure.Extenstions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IWeatherForecastService,WeatherForecastService>();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
